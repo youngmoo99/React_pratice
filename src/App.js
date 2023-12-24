ed0c8c36637bb78bbebc0903bd5c8032a7a865fd
youngmoo99 <rladuan612@gmail.com>
2023-12-24 16:41:36 +0900

    2days commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 
 import './App.css';
-import LearningComponent from './components/learning-examples/LearningComponent';
+import Counter from './components/counter/Counter';
+//import LearningComponent from './components/learning-examples/LearningComponent';
 
 
 function App() {
   return (
     <div className="App">
-      <LearningComponent/>
-    
+       <Counter/>    
     </div>
   );
 }
 
+// function PlayingWithProps(properties) {
+//   console.log(properties)
+//   console.log(properties.property1)
+//   console.log(properties.property2)
+//   return (
+//     <div>Props</div>
+//   )
+// }
 
+// function PlayingWithProps({property1,property2}) {
+//   console.log(property1)
+//   console.log(property2)
+
+//   return (
+//     <div>Props</div>
+//   )
+// }
 
 
 
